1ab381f8989ff40cd191bac2295fd5664c59ad2a
3hrsforsleep <rbznrzy@gmail.com>
2024-03-19 21:27:25 +0500

    hw_3.1.1

--- expencesCalculation.mjs
+++
@@ -1,6 +0,0 @@
-import { Calculation } from "./functions/calculationFunction.mjs";
-import { expencesExamples } from "./data/yearlyExpensesData.mjs";
-
-let expences = new Calculation;
-
-expences.expencesCalculation(expencesExamples)
